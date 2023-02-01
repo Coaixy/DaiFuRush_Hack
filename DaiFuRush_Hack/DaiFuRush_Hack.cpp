bc6993be255750999fe2b9b9fba41f2467e6f8d7
Coaixy <coaixy@outlook.com>
2023-02-01 11:32:55 +0800

    添加项目文件。

---
+++ DaiFuRush_Hack/DaiFuRush_Hack.cpp
@@ -0,0 +1,74 @@
+﻿// DaiFuRush_Hack.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "lib.h"
+#include <string>
+int main()
+{
+	std::string func_list[6] = {
+		"无限子弹","RPG","无敌","踏空跳","超级穿透","秒杀"
+	};
+	std::string on_list[6] = {
+		"关闭","关闭","关闭","关闭","关闭","关闭"
+	};
+	int length = sizeof(func_list) / sizeof(func_list[0]);
+	std::wstring game_name = L"DaifuRushByMutangM.exe";
+	DWORD game_id = FindProcessId(game_name);
+	HANDLE game_handle= getProcessHandle(game_id);
+	uintptr_t modBaseAddres = GetModuleBaseAddress(game_id, L"mono-2.0-bdwgc.dll");
+	exit(0);
+	Gui(func_list, on_list, length);
+
+
+	RegisterHotKey(GetActiveWindow(), 0, 0, VK_F1); //无限子弹
+	RegisterHotKey(GetActiveWindow(), 1, 0, VK_F2); //RPG
+	RegisterHotKey(GetActiveWindow(), 2, 0, VK_F3); //无敌
+	RegisterHotKey(GetActiveWindow(), 3, 0, VK_F4); //踏空
+	RegisterHotKey(GetActiveWindow(), 4, 0, VK_F5); //超级穿透
+	RegisterHotKey(GetActiveWindow(), 5, 0, VK_F6); //秒杀
+	RegisterHotKey(GetActiveWindow(), 6, 0, VK_F7); //退出
+	MSG msg = { 0 };
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		switch (msg.message)
+		{
+		case WM_HOTKEY:
+		{
+			if (msg.wParam == 0)
+			{
+				// "mono-2.0-bdwgc.dll"+007C2C08 198 4c0 38 20 9c
+			}
+			//198 4c0 39 20 7c
+			//198 4c0 30 20 110 55
+			//198 4c0 30 20 168
+			//198 4c0 38 20 8c
+			//198 4c0 38 20 84
+
+			if (msg.wParam == 6)
+			{
+				UnregisterHotKey(GetActiveWindow(), 0);
+				UnregisterHotKey(GetActiveWindow(), 1);
+				UnregisterHotKey(GetActiveWindow(), 2);
+				UnregisterHotKey(GetActiveWindow(), 3);
+				UnregisterHotKey(GetActiveWindow(), 4);
+				UnregisterHotKey(GetActiveWindow(), 5);
+				UnregisterHotKey(GetActiveWindow(), 6);
+				exit(0);
+			}
+		}
+		default:
+			break;
+		}
+	}
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ DaiFuRush_Hack/lib.h
@@ -0,0 +1,95 @@
+﻿#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+#include <string>
+
+DWORD FindProcessId(const std::wstring& processName)
+{
+	PROCESSENTRY32 processInfo;
+	processInfo.dwSize = sizeof(processInfo);
+
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (processesSnapshot == INVALID_HANDLE_VALUE)
+		return 0;
+
+	Process32First(processesSnapshot, &processInfo);
+	if (!processName.compare(processInfo.szExeFile))
+	{
+		CloseHandle(processesSnapshot);
+		return processInfo.th32ProcessID;
+	}
+
+	while (Process32Next(processesSnapshot, &processInfo))
+	{
+		if (!processName.compare(processInfo.szExeFile))
+		{
+			CloseHandle(processesSnapshot);
+			return processInfo.th32ProcessID;
+		}
+	}
+
+	CloseHandle(processesSnapshot);
+	return 0;
+}
+
+HANDLE getProcessHandle(DWORD pid) {
+	return OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+}
+
+void WriteMemory(HANDLE g_processHandle, void* value, DWORD valueSize, ...)
+{
+	if (value == NULL || valueSize == 0 || g_processHandle == NULL) return;
+
+	DWORD tempValue = 0;
+
+	va_list addresses;
+	va_start(addresses, valueSize);
+	DWORD offset = 0;
+	DWORD lastAddress = 0;
+	while ((offset = va_arg(addresses, DWORD)) != -1)
+	{
+		lastAddress = tempValue + offset;
+		::ReadProcessMemory(g_processHandle, (LPCVOID)lastAddress, &tempValue, sizeof(DWORD), NULL);
+	}
+	va_end(addresses);
+
+	::WriteProcessMemory(g_processHandle, (LPVOID)lastAddress, value, valueSize, NULL);
+}
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+	uintptr_t modBaseAddr = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
+		MODULEENTRY32 modEntry;
+		modEntry.dwSize = sizeof(modEntry);
+		if (Module32First(hSnap, &modEntry))
+		{
+			//std::cout << std::hex << (uintptr_t)modEntry.modBaseAddr << std::endl;
+			do
+			{
+				if (!_wcsicmp(modEntry.szModule, modName))
+				{
+					modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					break;
+				}
+			} while (Module32Next(hSnap, &modEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return modBaseAddr;
+}
+void Gui(std::string a[],std::string b[],int length) {
+	system("cls");
+	for (size_t i = 0; i < length; i++)
+	{
+		std::cout << "F" << i+1  << " " << a[i] << " " << b[i] << std::endl;
+	}
+	std::cout << "F7 退出" << std::endl;
+}
+void switchOn(std::string a[], std::string b[],int length) {
+
+}
